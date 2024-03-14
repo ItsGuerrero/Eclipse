d63776f4570027c6ea92f3a12a53ad093ab3be62
ItsGuerrero <jguerrerobelmonte@gmail.com>
2024-03-14 14:09:45 +0100

    añadimos persona

---
+++ persona/src/model/Model/Persona.java
@@ -0,0 +1,66 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persona {
+
+    /**
+     * Default constructor
+     */
+    public Persona() {
+    }
+
+    /**
+     * 
+     */
+    private String dni;
+
+    /**
+     * 
+     */
+    private String nombre;
+
+    /**
+     * 
+     */
+    private String apellido1;
+
+    /**
+     * 
+     */
+    private String apellido2;
+
+    /**
+     * 
+     */
+    private int edad = 0;
+
+    /**
+     * @param dni 
+     * @param nombre 
+     * @param apellido1 
+     * @param apellido2 
+     * @param edad
+     */
+    public void persona(String dni, String nombre, void apellido1, String apellido2, int edad) {
+        // TODO implement here
+    }
+
+    /**
+     * @param dni
+     */
+    public void setDni(String dni) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public void getDni() {
+        // TODO implement here
+        return null;
+    }
+
+}
